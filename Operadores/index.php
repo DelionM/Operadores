3b63dc8be5c31ce796d7811c10fb0111b3fab4bc
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 23:06:06 -0600

    Add files via upload

---
+++ Operadores/index.php
@@ -0,0 +1,10 @@
+<?php
+    #uso de operadore matematicos 
+    $a = 2;
+    $b = 6;
+    echo $c = $a * $b;
+    #Modulo / Residuo
+    echo "<br>";
+    echo "residio de 2 entre 6: ";
+    echo $d = $a % $b;
+?>
